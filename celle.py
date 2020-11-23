9e18c1658760a0e59201fdb7e47647a058e91e8b
BonfireHobo <72874118+BonfireHobo@users.noreply.github.com>
2020-11-23 23:21:18 +0100

    Estetisk update
    Bare for at det skulle være likt gjennom alle filene

--- celle.py
+++ celle.py
@@ -3,13 +3,16 @@ class Celle:
         # 0 = dead // 1 = alive
         self.status = 0
 
+
     # Change status to dead
     def set_dead(self):
         self.status = 0
 
+
     # Change status to alive
     def set_alive(self):
         self.status = 1
+
 
     # Check if cell is alive
     def is_alive(self):
@@ -17,6 +20,7 @@ class Celle:
             return True
         else:
             return False
+
 
     # Return  current sign for cell
     def get_sign(self):
