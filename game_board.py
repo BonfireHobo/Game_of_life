121c28f3712a230be9d492c27b81398d9be0910c
BonfireHobo <72874118+BonfireHobo@users.noreply.github.com>
2020-11-24 19:30:22 +0100

    Meduim change
    Changed lay up and added som comments

--- game_board.py
+++ game_board.py
@@ -3,13 +3,17 @@ from celle import Celle
 
 class GameBoard:
     def __init__(self, row, column):
-        # Game bords general info
+        # Game bords size
         self.row = row
         self.column = column
+
+        # Current generation
         self.generation = 0
 
-        # Create game borad
+        # List of the game board
         self.board = []
+
+        # Create game borad
         for _ in range(self.row):
             self.board_rows = []      
             for _ in range(self.column):
@@ -29,7 +33,7 @@ class GameBoard:
     def generate(self) :
         for i in range(self.row):
             for j in range(self.column):
-                birth_status = randint(0, 2)    # 1/3 Chance of cell being alive at birth
+                birth_status = randint(0, 2)  # 1/3 Chance of cell being alive at birth
                 if birth_status == 2:
                     self.board[i][j].set_alive()    
 
@@ -45,19 +49,31 @@ class GameBoard:
     # Update generation
     def update(self):
         # List of cells that will change status
-        self.new_dead_cells = []
-        self.new_alive_cells = []
+        new_dead_cells = []
+        new_alive_cells = []
 
         # Find neighbor to all cells
         for i in range(self.row):
             for j in range(self.column):
-                self.find_neighbor(i, j)
-                
+                # Current cell
+                cell_status = self.board[i][j].is_alive()
+
+                # Checking alive neighbors to current cell
+                alive_neighbors = self.find_neighbor(i, j)
+
+                # Checks game rules for living cell
+                if cell_status == True:
+                    if alive_neighbors < 2 or alive_neighbors > 3:
+                        new_dead_cells.append(self.board[i][j])
+                # Check game rules for dead cell
+                elif cell_status == False and alive_neighbors == 3:
+                    new_alive_cells.append(self.board[i][j])
+
         # Changing cell status
-        for cell in self.new_dead_cells:
+        for cell in new_dead_cells:
             cell.set_dead()
 
-        for cell in self.new_alive_cells:
+        for cell in new_alive_cells:
             cell.set_alive()
 
         # Update generations
@@ -66,27 +82,22 @@ class GameBoard:
 
     # Updates next generation of cells
     def find_neighbor(self, row, column):
-        # Variables related to curent cell
+        # Count of living neighbors
         alive_neighbors = 0
-        cell_status = self.board[row][column].is_alive()
 
         # Loop through neighbors
         for i in range(-1, 2):
             for j in range(-1, 2):
-                try:
-                    if i == 0 and j == 0:
+                try:  # Try to find alive neighbor except if neighbor dosent exis
+                    if i == 0 and j == 0:  # Make sure it does not count it self
                         continue
                     elif self.board[row + i][column + j].is_alive() == True:
                         alive_neighbors += 1
                 except IndexError:
                     continue
-
-        # Checks with game rules for cell
-        if cell_status == True:
-            if alive_neighbors < 2 or alive_neighbors > 3:
-                self.new_dead_cells.append(self.board[row][column])
-        elif cell_status == False and alive_neighbors == 3:
-            self.new_alive_cells.append(self.board[row][column])
+        
+        # Returns living neighbors
+        return alive_neighbors
 
 
     # Count current cells alive
